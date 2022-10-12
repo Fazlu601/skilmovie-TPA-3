d0488f7fe5f587d3bbfe04026affe37fc4a44a03
Fazlu601 <fazrlu9575@gmail.com>
2022-10-12 19:53:05 +0700

    fix date

--- source.js
+++ source.js
@@ -30,12 +30,12 @@ const renderMovie = (data) => {
     data['results'].reverse().forEach( item => {
         let card = `  <div class="card p-0 m-4 col-6 col-sm-5 col-md-4 col-lg-3 shadow-sm">
                         <img src="https://image.tmdb.org/t/p/w500/${item.backdrop_path == true ? item.backdrop_path : item.poster_path}" class="card-img-top img-fluid" alt="">
-                        <div class="card-body">
+                        <div class="card-body flex-column d-flex justify-content-between">
                             <div class="row pb-3">
                                 <div class="card-title text-wrap overflow-hidden col-9 h5">${item.original_title}</div>
                                 <div class="text-end col-3 h6">${item.vote_average}</div>
                             </div>
-                            <div class="row p-2 d-flex flex-column align-items-end>
+                            <div class="row >
                                 <p class="fs-5">${item.release_date}</p>
                             </div>
                         </div>
